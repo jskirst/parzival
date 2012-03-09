c872cbfca317bd628e41c24f7ec1af5fab45c408
Jonathan Kirst <jskirst@gmail.com>
2012-03-09 16:26:22 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parzival::Application.config.secret_token = '6770246197aab8b2e354a37db94de098724544482b4fe3cfc64ed4d5064e8676e2d082c5b730832d59f1f7b06c232bee04269951f7162f6ea3ccca83930a75bb'
